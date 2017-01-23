d5569b285cda632a89df904c7c1ae0e3e24e382e
Halak Serg <serghalak.gmail.com>
2017-01-23 18:38:16 +0200

    node iter and rec

--- src/mynode/lec4/TestNode.java
+++ src/mynode/lec4/TestNode.java
@@ -1,29 +1,61 @@
 package mynode.lec4;
 
+import java.util.Arrays;
+
 /**
  * Created by Serg on 22.01.2017.
  */
 public class TestNode {
     public static void main(String[] args){
         int[] n={1,2,3,4};
-        Node tail=createNode(n);
-        while(true){
-            if(tail.node==null){
-                System.out.println(tail);
-                System.out.println("It's head");
-                break;
-            }else{
-                System.out.println(tail);
-                tail=tail.node;
-            }
-        }
+        //Node tail=createNode(n);
+        Node tail=createNodeRec(n);
+        System.out.println(toStringRec(tail));
+//        while(true){
+//            if(tail.node==null){
+//                System.out.println(tail);
+//                System.out.println("It's head");
+//                break;
+//            }else{
+//                System.out.println(tail);
+//                tail=tail.node;
+//            }
+//        }
     }
     public static Node createNode(int[] n){
-        Node temp=new Node(n[0],null);
-        temp=new Node(n[1],temp);
-        temp=new Node(n[2],temp);
-        temp=new Node(n[3],temp);
-
+        Node temp=null;
+//                new Node(n[0],null);
+//        temp=new Node(n[1],temp);
+//        temp=new Node(n[2],temp);
+//        temp=new Node(n[3],temp);
+        for(int i=0;i<n.length;i++){
+            temp=new Node(n[i],temp);
+        }
         return temp;
+    }
+    public static Node createNodeRec(int[] n){
+        Node temp;
+        if(n.length==0) {
+            return null;
+        }else{
+            return new Node(n[0],createNodeRec(Arrays.copyOfRange(n,1,n.length)));
+        }
+    }
+
+    public static String toStringIter(Node tail){
+        String result="";
+        while(tail != null){
+            result +=tail.number + " -> ";
+            tail=tail.node;
+        }
+        return result +" * ";
+    }
+    public static String toStringRec(Node tail){
+
+        if(tail == null){
+            return  " * ";
+        }else{
+            return  tail.number +" -> " + toStringRec(tail.node);
+        }
     }
 }
